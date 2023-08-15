6fdc662c77b859f48d348d003bbd7db15c1ff6e0
鄭子洋 <a0661061@gmail.com>
2023-08-15 10:18:49 +0800

    [ADD] Self intro web

---
+++ Javascript/index.js
@@ -0,0 +1,57 @@
+$(function () {
+    AOS.init({
+        easing: 'ease-out-back',
+        duration: 1000
+    });
+    const swiper = new Swiper(".mySwiper", {
+        direction: "horizontal",
+        effect: "fade",
+        speed: 1200,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            dynamicBullets: true,
+            hideOnClick: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            hideOnClick: true,
+        },
+    });
+})
+
+document.onreadystatechange = function (e) {
+    if (document.readyState == "interactive") {
+        var all = document.getElementsByTagName("*");
+        for (var i = 0, max = all.length; i < max; i++) {
+            set_ele(all[i]);
+        }
+    }
+}
+
+function check_element(ele) {
+    var all = document.getElementsByTagName("*");
+    var totalele = all.length;
+    var per_inc = 100 / all.length;
+
+    if ($(ele).on()) {
+        var prog_width = per_inc + Number(document.getElementById("progress_width").value);
+        document.getElementById("progress_width").value = prog_width;
+        $("#bar1").animate({
+            width: prog_width + "%"
+        }, 3, function () {
+            if (document.getElementById("bar1").style.width == "100%") {
+                $(".progress").fadeOut("slow");
+            }
+        });
+    } else {
+        set_ele(ele);
+    }
+}
+
+function set_ele(set_element) {
+    check_element(set_element);
+}
